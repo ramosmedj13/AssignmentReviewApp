549cda8d899d2ae57e2bf90b1e6591137d8c1c9f
Joan Ramos <ramosjoanm13@gmail.com>
2023-06-28 14:15:57 -0400

    Test

--- src/main/java/com/hcc/controllers/AssignmentController.java
+++ src/main/java/com/hcc/controllers/AssignmentController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping
 public class AssignmentController {
     @Autowired
--- src/main/java/com/hcc/controllers/AuthController.java
+++ src/main/java/com/hcc/controllers/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/auth")
 public class AuthController {
     @Autowired
